b6d9a367a854b22187fe34d6807fa84c71759958
Alex Carvalho <acarvalh@usc.edu>
2017-07-05 13:34:15 +0200

    combined the dataframes and added map

--- csv/senseit data.R
+++ csv/senseit data.R
@@ -1,4 +1,5 @@
 library(readr)
+library(ggplot2)
 senseit_3_5 <- read_csv("senseit_3_5.csv")
 sep<- split(senseit_3_5, f=senseit_3_5[, "Sensor"])
 orientation=sep$`0`
@@ -11,7 +12,7 @@ location$Z = NULL
 colnames(location)[2] = "latitude" #add column names
 colnames(location)[3] = "longitude"
 
-fd1<- location %>% filter(latitude>-33.911994 & longitude>18.418049)
+fd1<- location %>% filter(latitude<-33.911994 & longitude>18.418049)
 foundry_data.frame<- filter(fd1, latitude> -33.912778 & longitude<18.419229)
 foundry_data.frame
 
@@ -23,18 +24,16 @@ rowcheck  <- function(foundry_data.frame, location){
 }
 
 match<- rowcheck(location, foundry_data.frame)
+match = as.data.frame(match)
+location = cbind(location, match)
+colnames(location)[4] = "area"
+location = location %>% mutate(area = ifelse(area == TRUE, "Foundry", "NA"))
 
-match
+ggplot(location, aes(x = latitude, y = longitude, color = area)) + geom_point(alpha = 0.5)
+ggplot(location, aes(x = area, fill = area)) + geom_bar() #we can visualize how much time a person spends at a 
 
-as.numeric(match)
-
-
-location[which(location[,3]< 234, arr.ind = TRUE), 5]<- "Foundry"
-
-location[which(location[,3]>233, arr.ind = TRUE), 5]<- NA
-
-mydata[which(mydata[,3]>500, arr.ind=TRUE), 3] <- NA
-       mydata[mydata$var3 > 500, 3] <- NA 
-
-#Write a function to see if you are within Foundry boundaries!
+#using Google Map API
+library(ggmap)
+cape_town = get_map(location = c(18.37,-33.92,18.470,-33.905), maptype = "roadmap")
+ggmap(cape_town) + geom_point(aes(x = longitude, y = latitude, color = area), data = location, size = 1)
 
